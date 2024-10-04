1a830306d15d8f4ed7dd66a1eb15e975c0dd3e45
seungjo <f1v3@kakao.com>
2024-10-04 21:23:51 +0900

    first commit

---
+++ src/test/java/com/f1v3/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
